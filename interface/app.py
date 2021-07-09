5eb30c099a2ca337f20dd1706ddcb85b233feb55
moroen <moroen@gmail.com>
2021-07-10 00:04:56 +0200

    tradfri and mqtt forms

--- interface/app.py
+++ interface/app.py
@@ -1,30 +1,39 @@
 # app.py
 #!bin/python
-from flask import Flask, request, render_template
-from forms import TradfriForm
+from flask import Flask, request, render_template, Markup
+from forms import TradfriForm, MQTTForm
 from flask_bs4 import Bootstrap
+
 app = Flask(__name__)
 
-app.config.from_mapping(
-    SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
+app.config.from_mapping(SECRET_KEY=b"\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G")
 Bootstrap(app)
-app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 app.config["BOOTSTRAP_USE_TOASTS"] = False
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET"])
 def registration():
-    form = TradfriForm(request.form)
-    if request.method == 'POST' and form.validate_on_submit():
-        return 'We confirm your registration!'
-    return render_template('preferences.html', form=form)
+    form_tradfri = TradfriForm(request.form)
+    form_mqtt = MQTTForm(request.form)
 
-@app.route('/mqtt', methods=['GET', 'POST'])
-def page_mqtt():
-    form = TradfriForm(request.form)
-    if request.method == 'POST' and form.validate_on_submit():
-        return 'We confirm your registration!'
-    return render_template('mqtt.html', form=form)
+    if form_tradfri.submit.data:
+        print("Tradfri")
+
+    if form_mqtt.submit.data:
+        print("mqtt")
+
+    # if request.method == "POST" and form_tradfri.validate_on_submit():
+    #    return "We confirm your registration!"
+
+    return render_template(
+        "preferences.html",
+        tradfri_active="active",
+        mqtt_active="",
+        form_tradfri=Markup(render_template("tradfri.html", form=form_tradfri)),
+        form_mqtt=Markup(render_template("mqtt.html", form=form_mqtt)),
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- interface/forms.py
+++ interface/forms.py
@@ -1,9 +1,14 @@
 # model.py
 from wtforms import SubmitField, BooleanField, StringField, PasswordField, validators
 from flask_wtf import FlaskForm
+
+
 class TradfriForm(FlaskForm):
-  ip = StringField('IP', 
-                 [validators.DataRequired()])
-  key = StringField('Key', 
-                 [validators.DataRequired()])
-  submit = SubmitField('Submit')
+    ip = StringField("IP", [validators.DataRequired()])
+    key = StringField("Key", [validators.DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class MQTTForm(FlaskForm):
+    host = StringField("Broker IP", [validators.DataRequired()])
+    submit = SubmitField("Submit")
